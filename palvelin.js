fe4e2a438a33d3f772fcfdc3b237d97c862e8302
Vampyyri <70080468+Vampyyri@users.noreply.github.com>
2023-01-19 23:08:11 +0200

    lisätty Get yhdelle leffalle

--- palvelin.js
+++ palvelin.js
@@ -1,4 +1,46 @@
+/*
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+*/
 'use strict';
 
 const axios = require('axios');
@@ -42,7 +84,7 @@ const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "Hollywood",
-    password: "",  // poistettu GitHubin versiosta
+    password: ,
     port: 5432,
 })
 
@@ -83,6 +125,26 @@ app.get('/films', async (request, response) => {
                 console.log(response)
                 var leffat = response.rows
                 console.log("leffat: ", leffat)
+                return leffat
+            })
+            .catch(error => console.error(error))
+        response.send(films)
+    } catch (error) {
+        response.json({ error: "jokin meni pieleen tietojen hakemisessa:" + error })
+    }
+
+
+})
+
+app.get('/film', async (request, response) => {
+    console.log("app.get")
+    try {
+        console.log("get films")
+        var films = await pool.query('SELECT * FROM films WHERE name=$1', [request.body.leffa])
+            .then(function (response) {
+                console.log(response)
+                var leffat = response.rows
+                console.log("leffa: ", leffa)
                 return leffat
             })
             .catch(error => console.error(error))
